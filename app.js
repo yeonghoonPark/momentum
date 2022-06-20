0905216ff49f5b71efcc099ce513e5ce8b3e26f2
yeonghoon Park <huklmnj2@naver.com>
2022-06-21 00:57:14 +0900

    login form

--- app.js
+++ app.js
@@ -5,18 +5,36 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username'
 
 const onLoginSubmit = (event) => {
+    
+    // prevent the refresh of the submit
     event.preventDefault();
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
-    const userName = loginInput.value;
+    const username = loginInput.value;
 
-    greeting.innerText = `Hello ${userName}`;
+    localStorage.setItem(USERNAME_KEY, `${username}`);
+
+    paintGreetings(username);
+
+}
+
+const paintGreetings = (parameter) => {
+    greeting.innerText = `Hello ${parameter}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener('submit', onLoginSubmit);
+if (savedUserName === null) {
+    // Show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    // Show greeting
+    paintGreetings(savedUserName);
+}
 
